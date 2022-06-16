90160647a88dce7633381e408d7c8474c7e10b67
da5idf <davidpforster24@gmail.com>
2022-06-16 06:52:50 -0400

    not found implemented

--- frontend/src/components/NotFound/NotFound.js
+++ frontend/src/components/NotFound/NotFound.js
@@ -1,9 +1,25 @@
 import React from "react";
 
+import "./NotFound.css"
+
 function NotFound() {
     return (
-        <div>
-            <img src={require("../../images/notFoundBanner.jpeg")} />
+        <div id="nf-hero" className="page-hero">
+            <div id="nf-page-content" className="page-content">
+                <div id="nf-content">
+                    <div id="nf-content-left">
+                        <div className="page-title">No vegetables here!</div>
+                    </div>
+                    <button id="nf-button" className="green-button">
+                        Go back to dashboard
+                    </button>
+                </div>
+                <div id="nf-img-container">
+                    <img id="nf-img" src={require("../../images/notFoundBanner.jpeg")} />
+
+                </div>
+
+            </div>
         </div>
     )
 }
