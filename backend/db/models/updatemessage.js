a53e31c5a036aa7e64f314862cf10bc4b91e7fe9
da5idf <davidpforster24@gmail.com>
2022-07-25 15:07:08 -0400

    formatting and debug

--- backend/db/models/updatemessage.js
+++ backend/db/models/updatemessage.js
@@ -2,6 +2,7 @@
 module.exports = (sequelize, DataTypes) => {
   const UpdateMessage = sequelize.define('UpdateMessage', {
     text: DataTypes.TEXT,
+    userId: DataTypes.INTEGER
   }, {});
   UpdateMessage.associate = function (models) {
     // associations can be defined here
--- frontend/src/components/UpdateMessage/UpdateMessage.js
+++ frontend/src/components/UpdateMessage/UpdateMessage.js
@@ -1,6 +1,7 @@
 import React from 'react'
 
 export default function UpdateMessage({ message }) {
+
     return (
         <div className="update-message-hero">
             <div>{new Date(message.createdAt).toDateString()}</div>
